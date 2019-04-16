5da1379478405af244050f046aa6478711360c93
wuzhenli <zhenli@6.cn>
2019-04-16 14:02:55 +0800

    自动合并不同架构 .a

--- BookManager/BookManager/Section/Home/ViewController.m
+++ BookManager/BookManager/Section/Home/ViewController.m
@@ -5,6 +5,7 @@
 //  Created by li’Pro on 2019/4/15.
 //  Copyright © 2019 JL. All rights reserved.
 //  https://www.jianshu.com/p/b6c59d8ed2c9
+//  自动合并不同架构的 .a : https://www.jianshu.com/p/c796fcba1604
 
 #import "ViewController.h"
 //#import <BookObtain/BookObtain.h>
@@ -27,6 +28,8 @@
     // Do any additional setup after loading the view.
     self.lblTitle.text = [BookObtain getBookName];
     self.lblTitle.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    
+    self.view.layer;
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
@@ -44,9 +47,16 @@
     CallingViewController *vc = [CallingViewController new];
     [[CallKit shareCallKit] presentCallViewController:vc];
 }
+
 - (IBAction)btnShowWindowsClicked:(id)sender {
     NSLog(@"****** windows ******");
     NSLog(@"%@", [UIApplication sharedApplication].windows);
+}
+
+- (void)testAnimation {
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position"];
+    
+    animation.additive;
 }
 
 
---
+++ BookObtain/BookObtainBundle/BookObtainBundle.h
@@ -0,0 +1,19 @@
+//
+//  BookObtainBundle.h
+//  BookObtainBundle
+//
+//  Created by li’Pro on 2019/4/16.
+//  Copyright © 2019 JL. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BookObtainBundle.
+FOUNDATION_EXPORT double BookObtainBundleVersionNumber;
+
+//! Project version string for BookObtainBundle.
+FOUNDATION_EXPORT const unsigned char BookObtainBundleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BookObtainBundle/PublicHeader.h>
+
+
